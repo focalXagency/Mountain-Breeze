183ef0b2a21f4af413013fba520098ec8bb5db2b
ali darwish <alidarwish502@gmail.com>
2023-09-14 13:56:27 +0300

    applied filtering for the gallery.

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,9 +1,11 @@
+const servicesHeader = document.querySelector(".services__header");
+
 const sections = document.querySelectorAll(".section");
 const circles = document.querySelectorAll(".circle");
 
 const sectionsArr = Array.from(sections);
 const circlesArr = Array.from(circles);
-console.log(circlesArr);
+// console.log(circlesArr);
 
 sections.forEach((section) => {
   section.addEventListener("click", () => {
@@ -17,9 +19,52 @@ sections.forEach((section) => {
     });
     let index = sectionsArr.indexOf(section);
     circlesArr[index].classList.add("circle--active");
+
+    const sectionName = section.dataset.section;
+    changeSection(sectionName);
+    getImagesAndVideos(sectionName)
   });
 });
 
+const changeSection = (section) => {
+  // console.log(sectionName);
+  const lastHeader = servicesHeader.getElementsByTagName("h2")[0];
+  const lastPragraph = servicesHeader.getElementsByTagName("p")[0];
+  const newHeader = document.createElement("h2");
+  const newPragraph = document.createElement("p");
+
+  changeSectionHeaderAndParagraph(section, newHeader, newPragraph);
+
+  // console.log(newHeader, newPragraph)
+  servicesHeader.replaceChild(newHeader, lastHeader);
+  servicesHeader.replaceChild(newPragraph, lastPragraph);
+};
+
+const changeSectionHeaderAndParagraph = (value, header, paragraph) => {
+  if (value === "Restaurant") {
+    header.innerText = "Our Restaurants";
+    paragraph.innerText = "Take a look on our restaurants";
+  }
+  if (value === "Chalet") {
+    header.innerText = "Our Chalets";
+    paragraph.innerText =
+      "Welcome to our cozy chalet where comfort and relaxation await.";
+  }
+  if (value === "Activity") {
+    header.innerText = "Our Activities";
+    paragraph.innerText = "Never stop your daily activity. ";
+  }
+  if (value === "Nature") {
+    header.innerText = "The Nature";
+    paragraph.innerText = "Nature is a source of wonder and inspiration.";
+  }
+  if (value === "Events") {
+    header.innerText = "Our Events";
+    paragraph.innerText = "Experience unforgettable moments with us.";
+  }
+};
+
+// **** Logic for scrolling ****
 const sectionsContainer = document.querySelector(".sections-container");
 // console.log(sectionsContainer);
 
@@ -42,3 +87,14 @@ sectionsContainer.addEventListener("mousemove", (e) => {
 sectionsContainer.addEventListener("mouseup", () => {
   isDragStart = false;
 });
+
+// **** End of logic for scrolling ****
+
+const getImagesAndVideos = (sectionName) => {
+  axios
+  .get("https://mountain.lavetro-agency.com/api/dashboard/galary", {params: {
+    type: sectionName
+  }})
+  .then((res) => console.log(res.data));
+
+};
